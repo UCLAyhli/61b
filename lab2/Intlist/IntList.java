98083633c1bb334b8b4aa56888994632a361fc85
UCLAyhli <yhli96@g.ucla.edu>
2022-03-20 17:34:46 -0700

    finish lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -1,3 +1,5 @@
+import jdk.internal.org.objectweb.asm.tree.InsnList;
+
 import java.util.Formatter;
 
 /**
@@ -82,7 +84,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList L = A;
+        while (L.rest != null) {
+            L = L.rest;
+        }
+        L.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +98,16 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList res = new IntList(A.first, null);
+        IntList ptr = res;
+        A = A.rest;
+        while (A != null) {
+            ptr.rest = new IntList(A.first, null);
+            A = A.rest;
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return res;
     }
 
 
